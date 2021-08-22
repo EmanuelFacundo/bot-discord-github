8b9877dea20d10e7e5ed653e0b4c193346569888
emanuelfacundo <87321905+emanuelfacundoink@users.noreply.github.com>
2021-08-22 19:29:23 -0300

    chore: message embed

--- src/bot/controller.js
+++ src/bot/controller.js
@@ -2,30 +2,59 @@ import { MessageEmbed } from "discord.js";
 import dotenv from "dotenv/config"
 
 import client from "./index.js";
+import constants from "../constants.js";
 
 const channel = () => client.channels.cache.get(process.env.CHANNEL_ID)
 
 
 export default async function webhook(req, res) {
   try {
-    var message = req.body
-    
-    
-    var messageEmbed = new MessageEmbed()
-      .setColor("#000000")
-      .setURL(message.pull_request.html_url)
-      .setAuthor(message.sender.login, message.sender.avatar_url)
-      .setTitle(`${message.pull_request.title} | `)
-      .setDescription(`${message.pull_request.body}`)
-      .addField("Reviewers", ".")
-      .setTimestamp()
+    const action = req.body.action 
+    const sender = req.body.sender
+    const pull_request = req.body.pull_request
+    const reviewers = req.body.pull_request.requested_reviewers
 
-    await channel().send({ embeds: [messageEmbed]})
-    res.status(200).send(messageEmbed)
+    if (!constants.actions.includes(action)) {
+      res.status(404).send("action not found")
+    }
 
+    const messageSend =  messageEmbed(action, sender, pull_request, reviewers)
+
+    await channel().send({ embeds: [messageSend]})
+
+    await channel().send(`${reviewers.map(reviewer => {
+      return constants.reviewers[reviewer.login]
+    })}`)
+    const clean = await channel().messages.fetch({limit: reviewers.length})
+    await channel().bulkDelete(clean)
+    
+    res.status(200).send({
+      message: "Message send"
+    })
+    
   } catch (error) {
     res.status(500).send({ 
       message: error.message
     })
   }
+}
+
+function messageEmbed(action, sender, pull_request, reviewers){
+  const colors = constants.colors
+
+  const messageEmbed =  new MessageEmbed()
+    .setColor(colors[action])
+    .setURL(pull_request.html_url)
+    .setAuthor(sender.login, sender.avatar_url)
+    .setTitle(`${pull_request.title} | ${action}`)
+    .setDescription(`${pull_request.body}`)
+    .setTimestamp()
+  
+  if (reviewers.length > 0){
+    messageEmbed.addField("Reviewers", `📌 ${reviewers.map(reviewer => {
+      return constants.reviewers[reviewer.login]
+    })}`)
+  }
+  
+  return messageEmbed
 }
---
+++ src/constants.js
@@ -0,0 +1,20 @@
+const constants = {
+  "colors": {
+    "opened": "#689F38",
+    "closed": "#EF6C00",
+    "review_requested": "#1565C0",
+    "review_request_removed": "#FF9800"
+  },
+  "actions": [
+    "opened",
+    "closed",
+    "review_requested",
+    "review_request_removed"
+  ],
+  "reviewers": {
+    "emanuelfacundoink": "<@266080109352583169>"
+  }
+
+} 
+
+export default constants
